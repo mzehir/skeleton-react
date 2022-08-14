e93b6f76c22001c2227870039a0e726217307fd4
Mümin <zehir.m.54@gmail.com>
2022-08-14 12:41:07 +0300

    fix

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,7 @@ import { dbConfig } from "../config";
 
 const axiosInstance = axios.create();
 
-axiosInstance.defaults.baseURL = `${dbConfig.REACT_APP_API_HOST}:${dbConfig.API_PORT}`;
+axiosInstance.defaults.baseURL = `${dbConfig.API_HOST}:${dbConfig.API_PORT}`;
 
 axiosInstance.interceptors.response.use(
   (response) => response,
