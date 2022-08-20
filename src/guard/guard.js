4c93f42544a6ca3eddd5a6be4bee105ada1ea640
Mümin <zehir.m.54@gmail.com>
2022-08-20 12:02:18 +0300

    fix

--- src/guard/guard.js
+++ src/guard/guard.js
@@ -1,16 +1,16 @@
 import * as React from "react";
-// import { Navigate, useLocation } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
-// import AuthUseContext from "../../hooks/AuthUseContext";
-// import { LOGIN } from "../../router/const";
+import AuthUseContext from "../hooks/auth-use-context";
+import { LOGIN_PAGE } from "../utils/constants/router-constants";
 
 function AuthGuard({ children }) {
-  //   let location = useLocation();
-  //   const { isInitialized, isAuthenticated } = AuthUseContext();
+  let location = useLocation();
+  const { isInitialized, isAuthenticated } = AuthUseContext();
 
-  //   if (isInitialized && !isAuthenticated) {
-  //     return <Navigate to={LOGIN.path} state={{ from: location }} replace />;
-  //   }
+  if (isInitialized && !isAuthenticated) {
+    return <Navigate to={LOGIN_PAGE.path} state={{ from: location }} replace />;
+  }
 
   return <React.Fragment>{children}</React.Fragment>;
 }
